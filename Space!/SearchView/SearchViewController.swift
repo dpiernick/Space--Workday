92638fa12541216982bd47dd28a46608c344717d
Dave Piernick <dpiernick@gmail.com>
2023-04-18 16:43:50 -0600

    create project

---
+++ Space!/SearchView/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  Space!
+//
+//  Created by Dave Piernick on 4/18/23.
+//
+
+import Foundation
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        view.backgroundColor = .black
+    }
+    
+}
